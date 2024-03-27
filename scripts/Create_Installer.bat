dcbd65aee9c1b09e3fdf4f8f304723d2d5e93d7f
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-03-27 16:38:22 +0200

    Rework installer

--- scripts/Create_Installer.bat
+++ scripts/Create_Installer.bat
@@ -18,23 +18,8 @@ FOR /F "tokens=*" %%i IN ('Tools\sigcheck.exe /accepteula /nobanner /n "..\MQTTP
 :: Temp xmp2 file
 copy /Y MQTTPlugin.xmp2 MQTTPluginTemp.xmp2
 
-:: Sed "{VERSION}" from xmp2 file
-Tools\sed.exe -i "s/{VERSION}/%version%/g" MQTTPluginTemp.xmp2
-
 :: Build MPE1
 "%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" MQTTPluginTemp.xmp2 /B /V=%version% /UpdateXML
 
 :: Cleanup
 del MQTTPluginTemp.xmp2
-
-:: Parse version (Might be needed in the futute)
-FOR /F "tokens=1-4 delims=." %%i IN ("%version%") DO ( 
-    SET major=%%i
-    SET minor=%%j
-    SET build=%%k
-    SET revision=%%l
-)
-
-:: Rename MPE1
-if exist "..\builds\MQTTPlugin-%major%.%minor%.%build%.%revision%.mpe1" del "..\builds\MQTTPlugin-%major%.%minor%.%build%.%revision%.mpe1"
-rename ..\builds\MQTTPlugin-MAJOR.MINOR.BUILD.REVISION.mpe1 "MQTTPlugin-%major%.%minor%.%build%.%revision%.mpe1"
